3bbcc2e340e2495e7323e1fa35d21105261fb5dc
zwz19920309 <939839685@qq.com>
2018-03-08 11:41:31 +0800

    fist commit

---
+++ config/db-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 禅道
+  MYSQL: {
+    IP: '119.29.112.251',
+    PORT: 3306,
+    USER: 'zhaizhenyong',
+    PWD: 'zhaizhenyong#M2017'
+  },
+   MYSQL_OT: {
+     IP: '10.68.3.88',
+     PORT: 3306,
+     USER: 'public',
+     PWD: 'public'
+   },
+  REDIS: {
+    PORT: 6379,
+    IP: '10.68.4.13',
+    PWD: 'Cninsure152'
+  }
+};
