8221c30dcb6b0d25a2c737b55028e9362aa9612c
zwz19920309 <939839685@qq.com>
2018-03-08 11:45:36 +0800

    Update db-config.js

--- config/db-config.js
+++ config/db-config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // 禅道
   MYSQL: {
-    IP: '119.29.112.251',
+    IP: '127.0.0.1',
     PORT: 3306,
     USER: 'zhaizhenyong',
     PWD: 'zhaizhenyong#M2017'
   },
    MYSQL_OT: {
-     IP: '10.68.3.88',
+     IP: '127.0.0.1',
      PORT: 3306,
      USER: 'public',
      PWD: 'public'
    },
   REDIS: {
     PORT: 6379,
-    IP: '10.68.4.13',
+    IP: '127.0.0.1',
     PWD: 'Cninsure152'
   }
 };
